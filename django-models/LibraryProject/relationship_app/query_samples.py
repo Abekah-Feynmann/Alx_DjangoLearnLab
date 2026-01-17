49039afa93997ba863d58290eaefc0ac215eddaf
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-17 02:27:51 +0000

    fixed an error for the list of all books

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,6 @@
 from .models import Book
 
 #List of all books in the library
-library = Library.objects.get(name="Abekah's Library")
+library_name = "Abekah's Library"
+library = Library.objects.get(name=library_name)
 all_books = Library.books.all()
