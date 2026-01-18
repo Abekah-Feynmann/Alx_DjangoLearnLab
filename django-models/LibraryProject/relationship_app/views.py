2c46573d296e6a301f3ae75dc14abb06779abbbc
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-18 20:34:17 +0000

    wrote the import statement in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth import UserCreationForm
 
 # Create a function-based view that list all books in the database.
 def list_books(request):
