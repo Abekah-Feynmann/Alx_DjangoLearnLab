16f812a7a2f26b98085d90b7e369460ba19b4099
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-18 20:27:02 +0000

    worked on urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import LibraryDetailView
 from .views import list_books
 
 urlpatterns = [
     path('relationship_app/', list_books), 
     path('relationship_app/', LibraryDetailView.as_view()),
+    path('relationship_app/', include('django.contrib.auth.urls'))
 ]
