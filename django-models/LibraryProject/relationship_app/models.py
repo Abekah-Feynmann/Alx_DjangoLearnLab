9409e6e4d341727ed992bda83903fbb9a3482041
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-19 16:39:57 +0000

    confused here

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -18,3 +18,7 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+class UserProfile(models.Model):
+    user = 
+    role = 
