787b813c16e5ef07c509bbe8dcb0f224b120f9e7
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-20 18:05:07 +0000

    removed extra and worked on member_view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -49,6 +49,18 @@ def admin_view(request):
     return render(request, 'admin_view.html')
 
 
+#the role checking function
+def is_member(user):
+    return(
+        user.is_authenticated and
+        user.userprofile.role == 'Member'
+    )
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
+
+
     
 
 
