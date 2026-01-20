466263a50878355786ba8295489dc9f3d56ef290
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-20 18:08:10 +0000

    worked on the template path for the role based apps

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -46,7 +46,7 @@ def is_admin(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'templates/relationship_app/admin_view.html')
 
 
 #the role checking function
@@ -58,7 +58,7 @@ def is_member(user):
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'templates/relationship_app/member_view.html')
 
 
     
