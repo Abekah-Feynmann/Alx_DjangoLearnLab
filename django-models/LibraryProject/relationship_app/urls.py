8046c2df27aa7c282e83e8106285cc97b319131c
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-21 19:10:48 +0000

    linked the urls in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from .views import LibraryDetailView
-from .views import list_books, admin_view, member_view, librarian_view,
+from .views import list_books, admin_view, member_view, librarian_view, add_book, delete_book, change_book
 
 
 urlpatterns = [
-    path('relationship_app/', list_books), 
+    path('relationship_app/', list_books, name='list_books'), 
     path('relationship_app/', LibraryDetailView.as_view()),
     path('relationship_app/', include('django.contrib.auth.urls')), 
     path('relationship_app/login', LoginView.as_view(template_name='relationship_app/login.html', name='login')),
@@ -13,4 +13,7 @@ urlpatterns = [
     path('relationship_app/', admin_view), 
     path('relationship_app/', member_view),
     path('relationship_app/', librarian_view),
+    path('relationship_app/edit_book', change_book),
+    path('relationship_app/add_book', add_book),
+    path('relationship_app/delete_book', delete_book),
 ]
