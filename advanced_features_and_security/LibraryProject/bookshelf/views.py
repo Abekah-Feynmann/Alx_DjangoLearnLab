8c63f78a5c2d12d12118649246688147eb2ef6fb
abekahFeynmann <abekaharthur5966@gmail.com>
2026-01-24 12:05:46 +0000

    worked up to a point

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
-from django.contrib.auth.models import Permission
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
 
 # Create your views here.
-#Defining Custom Permissions
+class Command(BaseCommand):
+    help = 'Create user groups and assign permissions'
 
-#get permissiion
-permission = Permission.objects.filter(
-    codename__in=['can_view', 'can_edit', 'can_create', 'can_delete'])
-CustomUser.user_permissions.add(permission)
+    def handle(self, *args, **kwargs):
+        editor, _ = Group.objects.get_or_create(name='Editor')
+        viewer, _ = Group.objects.get_or_create(name='Viewer')
+        admin, _ = Group.objects.get_or_create(name='Admin')
+
+        editor_perms = Permission.objects.filter(
+            codename__in = ["can_edit", "can_create"]
+        )
+        viewer_perms = Permission.objects.filter(
+            codename__in = ["can_view"]
+        )
+        admin_perms = Permission.objects.filter(
+            codename__in = ["can_delete", "can_create", "can_edit", "can_view"]
+        )
+
+        editor.permissions.set(editor_perms)
+        viewer.permissions.set(viewer_perms)
+        admin.permissions.set(admin_perms)
+
+        self.stdout.write(self.style.SUCCESS("Groups and permissions created"))
+
+        
+
+
+
+
+    
+
