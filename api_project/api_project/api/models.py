24b1d1d78d861f3d05242c4a43c1b1a9ddc76528
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-01 17:19:45 +0000

    rectified an error in settings.py; a comma and ensured that the app is running smoothly

--- api_project/api_project/api/models.py
+++ api_project/api_project/api/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 #A book model with title and author
 class Book(models.Model):
-    title = model.CharField(max_length=200)
-    author = model.CharField(max_length=100)
-    
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+
     
 
