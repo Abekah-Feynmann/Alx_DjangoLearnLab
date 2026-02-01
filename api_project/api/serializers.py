0046e160370d40ff61bfcc95c35ead6bee581c69
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-01 20:25:03 +0000

    defined a view for the BookSerializer in views.py

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -2,7 +2,9 @@ from .models import Book
 from rest_framework import serializers
 
 #A book serializer class that includes all fields of the book model
-class BookSerializers(serializers.ModelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['title', 'author']
+
+
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework import generics
+
 
 # Create your views here.
+#Defines a view that uses BookSerializer to return and retrieve book data
+class Booklist(generics.ListAPIView):
+    queryset = Book
+    serializer_class = BookSerializer
