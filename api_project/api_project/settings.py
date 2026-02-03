8ddf95778ef186b522ba0a75ab1169f83e410dfc
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-03 16:54:09 +0000

    added permissions.IsAuthenticated and IsAdminUser to default authentication classes in settings

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -123,5 +123,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK  = {
     'DEFAULT_AUTHENTICATION_CLASSES' : [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
     ]
 }
