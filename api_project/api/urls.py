52eb10cd94010a5bd880c893d09e9c2d312ae884
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-03 16:47:38 +0000

    modified BookViewSet to include permissions for isauthenticated and isadminuser

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -12,4 +13,8 @@ urlpatterns = [
 
     #Include Router urls for BookViewSet (all CRUD operations)
     path('', include(router.urls)),
+
+    #A mechanism for users to obtain a token given their username and password
+    path('api-token-auth/', views.obtain_auth_token),
+
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 
 # Create your views here.
@@ -15,6 +17,7 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 
 
