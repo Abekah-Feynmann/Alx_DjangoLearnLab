a2332bb0df3aa8daac32f3e5880d033abf2455eb
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-08 10:44:02 +0000

    created more endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,5 +11,8 @@ router.register('books', ListView, basename=None)
 #Connecting views with specific endpoints
 
 urlpatterns = [
-    path('/books/', ListView.as_view(), name='book-list')
+    path('/books/', ListView.as_view(), name='book-list'),
+    path('books/retrieve', DetailView.as_view(), name='book-detail'),
+    path('books/delete', DeleteView.as_view(), name='book-delete'),
+    path('books/delete', CreateView.as_view(), name='book-create'),
 ]
