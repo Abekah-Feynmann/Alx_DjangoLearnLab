84eb39ba36ab0508d185d2e5dbd3c75da44867fb
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-08 10:46:59 +0000

    finished endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,6 @@
 from .models import Book
 from django.urls import path
-from .views import ListView, DetailView, DeleteView, UpdateView
+from .views import ListView, DetailView, DeleteView, UpdateView, CreateView
 from rest_framework.routers import DefaultRouter
 
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('/books/', ListView.as_view(), name='book-list'),
     path('books/retrieve', DetailView.as_view(), name='book-detail'),
     path('books/delete', DeleteView.as_view(), name='book-delete'),
-    path('books/delete', CreateView.as_view(), name='book-create'),
+    path('books/create', CreateView.as_view(), name='book-create'),
+    path('books/update', UpdateView.as_view(), name='book-update'),
 ]
