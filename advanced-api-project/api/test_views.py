0b56c14e5ecc8c1a22c37e14523a0091ed87cfd3
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-11 19:23:59 +0000

    wrote a proper test for BookTestCase

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,10 +1,13 @@
-from django.test import TestCase
 from .models.py import Book
+from rest_framework.tests import APITestCase
 
-class BookTestCase(tests.unittest):
+class BookTestCase(APITestCase):
     def test_create(self):
         Book.objects.create(author='Peggy Oppong', title="End of the tunnel" )
-        self.assertEqual(book.title, "End of the tunnel")
+        response = self.client.get('api/books/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
 
     def test_update(self):
         author_name = Book.objects.get(author= "Peggy Oppong", title="End of the tunnel")
+        self.assertEqual()
