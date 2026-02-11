31192e8cf1d03acdfe700cbc4e1c4c420e6ea1a3
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-11 19:04:37 +0000

    writing unit test for createview and detailview, on the latter

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,2 +1,10 @@
 from django.test import TestCase
 from .models.py import Book
+
+class BookTestCase(tests.unittest):
+    def test_create(self):
+        Book.objects.create(author='Peggy Oppong', title="End of the tunnel" )
+        self.assertEqual(book.title, "End of the tunnel")
+
+    def test_update(self):
+        author_name = Book.objects.get(author= "Peggy Oppong", title="End of the tunnel")
