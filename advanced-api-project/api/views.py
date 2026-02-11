53909670112bcaa6db43b23dda94bfd61b6a8c10
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-11 05:33:26 +0000

    changed the import statement for filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics, filters
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 from rest_framework.exceptions import PermissionDenied
+from django_filters import rest_framework
 
 
 
