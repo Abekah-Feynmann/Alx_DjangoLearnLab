37a5d1956f9c09cd55878122632e6baba529fefc
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-13 05:19:39 +0000

    re-wrote the BookTestCase function

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,14 +1,28 @@
 from .models.py import Book
-from rest_framework.tests import APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 from rest_framework import status
+from .views import CreateView
+
+
+factory = APIRequestFactory()
 
 class BookTestCase(APITestCase):
     def test_create(self):
-        Book.objects.create(author='Peggy Oppong', title="End of the tunnel" )
-        response = self.client.get('api/books/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
+        request = factory.post('api/books/', {
+            author: 'Peggy Oppong',
+            title: "End of the tunnel"
+
+        }, format= 'json')
+        view = CreateView.as_view()
+        response = view(request)
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Book.objects.count(), 1)
 
     def test_update(self):
         author_name = Book.objects.get(author= "Peggy Oppong", title="End of the tunnel")
-        self.assertEqual()
+        response = self.client.get('books/update')
+        self.assertEqual(response_code, 200)
+
+    def test_delete(self):
+        
