2857b8fcb7bd9b8f02bbd72f8d49e18e4c7f5c24
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-14 16:23:56 +0000

    set up views for authentication

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+#Create a custom UserCreationForm to include email
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True) 
+    class Meta:
+        model = User
+        fields = ("Username", "Password", "Email")
+
+    
+# Creating the signup view (User Registration)
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+#Creating the login view
+class LoginView(views.LoginView):
+
+
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('django.contrib.auth.urls')),
+    path('blog/profile', TemplateView.as_view(template_name='blog/profile.html'), name='profile'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
