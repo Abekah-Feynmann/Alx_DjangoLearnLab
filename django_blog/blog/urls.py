4670ac7bc8ba7a1902ba3e1aa5ecc35e865ba02b
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-14 16:37:56 +0000

    created the urls.py for blog app

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+urlpatterns = [
+    path('profile/', TemplateView.as_view(template_name='blog/profile.html'), name='profile'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+]
