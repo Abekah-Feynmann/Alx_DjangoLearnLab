0c8b6c0e71cd1ea36d3adf1d7d7c0a965020d06c
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-15 08:31:53 +0000

    rectified the order of <int:pk> in urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('post-list/', PostListView.as_view(), name='post-list'),
     path('post-detail/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post-create/', PostCreateView.as_view(), name='post-create'),
-    path('post-update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
-    path('post-delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 ]
