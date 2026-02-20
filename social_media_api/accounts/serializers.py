e0e9bbeebadc78ee1a07e2ab19a9e3941e2d1f58
abekahFeynmann <abekaharthur5966@gmail.com>
2026-02-20 08:19:56 +0000

    Created the serializers and views for login, register and profile

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,63 @@
 from .models import CustomUser
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
 #Create serializers for CustomUser model
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
+
+"""
+    Create serializers for registeration
+"""
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={"Input_type: password"})
+    password2 =  serializers.CharField(write_only=True, required=True, style={"Input_type: password"})
+
+    class Meta:
+        model = CustomUser
+        fields = '__all__'
+
+    #validating the data
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            return serializers.ValidationError("Password: Passwords do not match")
+        return data
+
+    #creating the user
+    def create(self, validated_data):
+        validated_data.pop('password2')
+
+        user = User.objects.create(
+            username = validated_data['username'],
+            email = validated_data.get("email"),
+            password = validated_data['password']
+        )
+
+        return user
+
+
+"""
+    Creating the serializer for login
+"""
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+"""
+    Creating the serializer for Profile
+"""
+
+class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ["id", "profile_picture,", "bio", "followers"]
+
+
+
+
+
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
+
+
 
 # Create your views here.
+
+#Creating a registration view
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data= request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "user registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# creating the login view
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data = request.data)
+
+        if serializer.is_valid():
+            username = serializer.validated_data["username"]
+            password = serializer.validated_data["password"]
+
+            #checks if user exists and creates a user object if authentication check is run successfully
+            user = authenticate(username=username, password=password)
+        return Response(serializer.errors, status=400)
+        
+        #Generate token based on the existence of a user
+        if user is None:
+            return Response({"error" : "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({"message" : "Login Successful"}, {"token" : token.key})  
+
+#creating the profile view
+class ProfileView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(user=request.user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Profile updated successfully",
+                            "data" = serializer.data}, status = status.HTTP_200_OK                
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
