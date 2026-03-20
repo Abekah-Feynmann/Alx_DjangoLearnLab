9b9e545ebc58be29d27933d77998a6749aa549b8
abekahFeynmann <abekaharthur5966@gmail.com>
2026-03-20 07:22:55 +0000

    changed followers to following and worked on the related_name in my accounts model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,29 +7,10 @@ from django.conf import settings
 class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
-    followers = models.ManyToManyField("self", symmetrical=False, related_name = "following", blank=True)
+    following = models.ManyToManyField("self", symmetrical=False, related_name = "followers", blank=True)
 
     def __str__(self):
         return self.username
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Comment by {self.author}"
 
 
