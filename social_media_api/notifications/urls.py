0923cefeea4e2445db6bc841e71243399b0aee72
abekahFeynmann <abekaharthur5966@gmail.com>
2026-03-26 22:37:07 +0000

    worked on notificationss for lke, follows and clear clear

---
+++ social_media_api/notifications/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("notifications/", ) #up_next -> setting up a notification system
+]
--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Notification
 
 # Create your views here.
+class NotificationListView(generics.ListAPIView):
+    queryset = Notification.objects.all
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(recipient= self.request.user)
+
+
